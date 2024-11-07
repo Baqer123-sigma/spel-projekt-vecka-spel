a83840b595166df763eca79f4b95ba12a9041e8a
Mohammedbaqer Al-Ghalibi <mohammedbaqer.algha@learnet.se>
2024-11-07 13:53:45 +0100

    Those who kiss loke

--- Assets/PlayerMovementSound.cs
+++ Assets/PlayerMovementSound.cs
@@ -1,18 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerMovementSound : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public AudioSource audioSource;  // Attach the audio source in the inspector
+    public AudioClip movementSound;  // Assign your sound clip here
+    public float soundInterval = 0.5f;  // Time between each sound in seconds
+
+    private float timeSinceLastSound = 0f;
+
     void Start()
     {
-        
+        if (audioSource == null)
+        {
+            audioSource = GetComponent<AudioSource>();
+        }
+
+        // Set the clip and make sure it doesn't play on awake
+        if (audioSource != null)
+        {
+            audioSource.clip = movementSound;
+            audioSource.playOnAwake = false;
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        bool isMoving = Mathf.Abs(Input.GetAxis("Horizontal")) > 0.1f || Mathf.Abs(Input.GetAxis("Vertical")) > 0.1f;
+
+        if (isMoving && timeSinceLastSound >= soundInterval)
+        {
+            audioSource.PlayOneShot(movementSound);
+            timeSinceLastSound = 0f;
+        }
+
+        timeSinceLastSound += Time.deltaTime;
     }
 }
